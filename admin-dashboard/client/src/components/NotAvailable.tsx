162cca87ab4b86d1140fdf7c6d27127db8c34746
Anubhaw Raman <anubhawraman@gmail.com>
2024-10-25 03:51:59 +0530

    Admin frontend / personal info page complete

--- admin-dashboard/client/src/components/NotAvailable.tsx
+++ admin-dashboard/client/src/components/NotAvailable.tsx
@@ -4,7 +4,7 @@ export default function NotAvailable({message, button}: {message: string, button
     return (
         <div className="w-full flex flex-col justify-center items-center">
             <h1 className="text-red-600 bg-inherit text-4xl font-medium h-1/7 flex justify-center items-center bg-white p-6 rounded-2xl shadow-xl">{message}</h1>
-            <Button className="mt-10 bg-blue-500 text-white text-lg font-semibold hover:bg-blue-700 shadow-xl transition-colors duration-50">{button}</Button>
+            <Button className="mt-10 bg-blue-600 text-white text-lg font-semibold hover:bg-blue-800 shadow-xl transition-colors duration-50">{button}</Button>
         </div>
     )
 }
